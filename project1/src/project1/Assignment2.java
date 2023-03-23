b2423deb566e5baa4cd1e36a70c33097b3ddb92c
Hp <Hp@DESKTOP-5FAKU67>
2023-03-23 13:23:27 +0530

    pushing for 1st commit

---
+++ project1/src/project1/Assignment2.java
@@ -0,0 +1,38 @@
+package project1;
+
+public class Assignment2 {
+
+	public Assignment2 ()
+	{
+    this(1,2,3,4);
+	System.out.println("default constructer");	
+	}
+
+	public Assignment2 (int a)
+	{
+		this();
+	System.out.println("1 para");	
+	}
+
+	public Assignment2 (int a,int b)
+	{
+		this(1,1,1);
+	System.out.println("2 para");	
+	}
+	
+	public Assignment2 (int a,int b,int c)
+	{
+		this(1);
+	System.out.println("3 para");	
+	}
+	
+	public Assignment2 (int a,int b,int c,int d)
+	{
+	System.out.println("4 para");	
+	}
+	
+	public static void main(String[] args) {
+		Assignment2 callC=new Assignment2(1,2);
+	}
+}
+
